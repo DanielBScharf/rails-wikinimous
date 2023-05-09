5f3c507d10c12c978dfb8f60073ddb5793c1c5c7
Daniel Scharf <id.scharfd@gmail.com>
2023-05-09 17:11:35 +0900

    articles are searchable, not pretty yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/articles/new', to: 'articles#new', as: :new_article
+  get '/articles/index', to: 'articles#index', as: :articles
+  post '/articles', to: 'articles#create'
 
+  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  get '/articles/:id', to: 'articles#show', as: :article
+  patch '/articles/:id', to: 'articles#update'
+
+  delete '/articles/:id', to: 'articles#destroy'
   # Defines the root path route ("/")
   # root "articles#index"
 end
